53c1199c6f39214c7cb6d57450613293db8d461f
Ivan Wills <ivan.wills@gmail.com>
2015-06-13 17:02:15 +1000

    Updating for release of version 0.0.2

--- lib/App/aptperl.pm
+++ lib/App/aptperl.pm
@@ -14,7 +14,7 @@ use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
 use base qw/Exporter/;
 
-our $VERSION     = version->new('0.0.1');
+our $VERSION     = version->new('0.0.2');
 our @EXPORT_OK   = qw//;
 our %EXPORT_TAGS = ();
 
@@ -28,7 +28,7 @@ App::aptperl - Installs perl dependencies from apt (if possible) otherwise uses 
 
 =head1 VERSION
 
-This documentation refers to apt-perl version 0.0.1
+This documentation refers to apt-perl version 0.0.2
 
 =head1 SYNOPSIS
 
